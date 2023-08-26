913f6bddb0244eb115fd0362b5aa85e30edd74e0
cwl <3503127564@qq.com>
2023-08-26 14:21:31 +0800

    动画效果的添加，css的一些优化

---
+++ assets/js/preserve-3d.js
@@ -0,0 +1,24 @@
+const multiple = 20;
+const mouseOverContainer = document.getElementsByTagName("body")[0];
+const element = document.getElementById("preserve-3d");
+
+function transformElement(x, y) {
+  let box = element.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / multiple;
+  let calcY = (x - box.x - (box.width / 2)) / multiple;
+  
+  element.style.transform  = "rotateX("+ calcX +"deg) "
+                        + "rotateY("+ calcY +"deg)";
+}
+
+mouseOverContainer.addEventListener('mousemove', (e) => {
+  window.requestAnimationFrame(function(){
+    transformElement(e.clientX, e.clientY);
+  });
+});
+
+mouseOverContainer.addEventListener('mouseleave', (e) => {
+  window.requestAnimationFrame(function(){
+    element.style.transform = "rotateX(0) rotateY(0)";
+  });
+});
